d362b09facc1c0913bae70bf2b5894e426ee5c5d
HelloWorldProb <davidlubka80@gmail.com>
2023-03-15 16:19:17 +0100

    MainComit

--- GitHubP/src/Main.java
+++ GitHubP/src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println(sumar(10, 5));
+        System.out.print("Nice to meet you");
     }
 
     public static int sumar(int x, int y) {
